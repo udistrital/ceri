1fc3d7ce50e69c56a76c0d17d4a40ad8ba56f573
nicolas0517 <nicolasbernal17@gmail.com>
2017-06-20 15:06:52 -0500

    arreglo diseño componente docentes entrantes

--- src/app/components/docentes-entrantes/docentes-entrantes.component.ts
+++ src/app/components/docentes-entrantes/docentes-entrantes.component.ts
@@ -6,6 +6,31 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: 'docentes-entrantes.component.html'
 })
 export class DocentesEntrantesComponent implements OnInit {
+
+    identificacion = {
+        'numero': null
+    };
+
+    datos_persona = {
+        'identificacion': 20121020079,
+        'nombres': 'Catalina',
+        'apellidos': 'Cano',
+        'pais_procedencia': 'Argentina'
+    };
+
+    datos_movilidad = {
+        'tipo_movilidad': 'nacional',
+        'fecha_inicio': '',
+        'fecha_fin': '',
+        'periodo_estancia': 'corto',
+        'categoria_movilidad': 1,
+        'dependencia': 1,
+        'area_conocimiento': '',
+        'concepto_participacion': '',
+        'presupuesto': null,
+        'acto_administrativo': null
+    };
+
     constructor() { }
 
     ngOnInit() {}
